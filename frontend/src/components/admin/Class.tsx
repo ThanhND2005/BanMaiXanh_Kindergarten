f9e583a845c3f1b033b1ee50e52a111cace42bf0
danhthanh <nguyendanht2005@gmail.com>
2026-01-28 21:13:37 +0700

    update 28/1/2026

--- frontend/src/components/admin/Class.tsx
+++ frontend/src/components/admin/Class.tsx
@@ -1,12 +1,349 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { ChevronsUpDown } from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+const ClassFormSchema = z.object({
+  imageurl: z
+    .any()
+    .refine((file) => file?.length > 0, {
+      message: "Vui lòng tải ảnh đại diện lớp học",
+    }),
+  name: z.string().min(1, "Tên lớp không được để trống"),
+  teacherid: z.string().min(1, "Vui lòng chọn giáo viên"),
+  age: z.number("Vui lòng nhập đúng định dạng"),
+  members: z.number("Vui lòng nhập đúng định dạng"),
+  tuition: z.number("Vui lòng nhập đúng định dạng"),
+  schedule: z.string().min(1, "Vui lòng nhập lịch học cho lớp"),
+  classtype: z.string(),
+});
+type Teacher = {
+  teacherid: string;
+  name: string;
+};
+type ClassCard = {
+  classid: string;
+};
 
+type ClassFormValues = z.infer<typeof ClassFormSchema>;
+const teachers: Teacher[] = [
+  {
+    teacherid: "001",
+    name: "Nguyen Thi Van",
+  },
+  {
+    teacherid: "002",
+    name: "Nguyen Thi Yen",
+  },
+  {
+    teacherid: "003",
+    name: "Nguyen Thi Oanh   ",
+  },
+];
+const classes = [
+  {
+    classid: "001",
+    name: "Mam 1",
+    age: 3,
+    members: 16,
+    currentmambers: 12,
+    tuition: 1600000,
+    teachername: "Nguyen Thi Van",
+    teacherid:'001',
+    imageurl:
+      "https://res.cloudinary.com/dhylrhxsa/image/upload/v1769583472/51758c8655e6dbb882f7_dqoijs.jpg",
+  },
+  {
+    classid: "002",
+    name: "Mam 1",
+    age: 3,
+    members: 16,
+    currentmambers: 12,
+    tuition: 1600000,
+    teacherid:'002',
+    teachername: "Nguyen Thi Van",
+    imageurl:
+      "https://res.cloudinary.com/dhylrhxsa/image/upload/v1769583472/51758c8655e6dbb882f7_dqoijs.jpg",
+  },
+  {
+    classid: "003",
+    name: "Mam 1",
+    age: 3,
+    members: 16,
+    currentmambers: 12,
+    tuition: 1600000,
+    teacherid:'003',
+    teachername: "Nguyen Thi Van",
+    imageurl:
+      "https://res.cloudinary.com/dhylrhxsa/image/upload/v1769583472/51758c8655e6dbb882f7_dqoijs.jpg",
+  },
+  {
+    classid: "004",
+    name: "Mam 1",
+    age: 3,
+    members: 16,
+    currentmambers: 12,
+    tuition: 1600000,
+    teachername: "Nguyen Thi Van",
+    teacherid: '004',
+    imageurl:
+      "https://res.cloudinary.com/dhylrhxsa/image/upload/v1769583472/51758c8655e6dbb882f7_dqoijs.jpg",
+  },
+];
 const Class = () => {
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClassFormValues>({
+    resolver: zodResolver(ClassFormSchema),
+  });
+  const {
+    register: reg,
+    handleSubmit: had,
+    formState: { errors: err, isSubmitting: isSub },
+  } = useForm<ClassCard>();
+  const onSubmit1 = async (data: ClassFormValues) => {};
+  const onSubmit2 = async (data: ClassCard) => {};
+  const [open, setOpen] = useState(false);
+  const [teacherid, setTeacherId] = useState("");
   return (
-    <div>
+    <>
+      <div className="w-full flex justify-between">
+        <h1 className="text-2xl itim-regular">Các lớp học trong hệ thống:</h1>
+        <div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant="outline"
+                className="w-full rounded-2xl bg-[#05D988] text-[#ffffff] hover:bg-[#02B671] focus:bg-[#05D988]  transition all"
+              >
+                Tạo lớp học
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <h1 className="text-2xl font-bold text-center">Tạo lớp học</h1>
+              <form
+                className="flex flex-wrap mt-2 gap-2"
+                onSubmit={handleSubmit(onSubmit1)}
+              >
+                <div className="w-full">
+                  <Label htmlFor="imageurl" className="text-sm block">
+                    Ảnh lớp học
+                  </Label>
+                  <Input
+                    type="file"
+                    id="imageurl"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Ảnh lớp học"
+                    {...register("imageurl")}
+                  />
+                  {errors.imageurl && (
+                    <p className="text-destructive text-sm">
+                      {errors.imageurl.message as string}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="name" className="text-sm block">
+                    Tên lớp
+                  </Label>
+                  <Input
+                    type="text"
+                    id="name"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Nhập tên lớp"
+                    {...register("name")}
+                  />
+                  {errors.name && (
+                    <p className="text-destructive text-sm">
+                      {errors.name.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <Input
+                    type="hidden"
+                    id="teacherid"
+                    value={teacherid}
+                    {...register("teacherid")}
+                  />
+                  {errors.teacherid && (
+                    <p className="text-destructive text-sm">
+                      {errors.teacherid.message}
+                    </p>
+                  )}
+                  <Label htmlFor="teacherid" className="text-sm block">
+                    Giáo viên
+                  </Label>
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={open}
+                        className="w-full flex justify-between rounded-xl border border-[#000000] shadow-md"
+                      >
+                        {teacherid
+                          ? teachers.find(
+                              (teacher: Teacher) =>
+                                teacher.teacherid === teacherid,
+                            )?.name
+                          : "Chọn giáo viên"}
+                        <ChevronsUpDown className="opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <Command>
+                        <CommandList>
+                          <CommandEmpty>
+                            Không có giáo viên phù hợp
+                          </CommandEmpty>
+                          <CommandGroup>
+                            {teachers.map((teacher) => (
+                              <CommandItem
+                                key={teacher.teacherid}
+                                value={teacher.teacherid}
+                                onSelect={(currentId) => {
+                                  setTeacherId(
+                                    currentId === teacherid ? "" : currentId,
+                                  );
+                                  setOpen(false);
+                                }}
+                              >
+                                {teacher.name}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="age" className="text-sm block">
+                    Độ tuổi
+                  </Label>
+                  <Input
+                    type="text"
+                    id="age"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Nhập độ tuổi"
+                    {...register("age")}
+                  />
+                  {errors.age && (
+                    <p className="text-destructive text-sm">
+                      {errors.age.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="members" className="text-sm block">
+                    Sổ trẻ
+                  </Label>
+                  <Input
+                    type="text"
+                    id="members"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Nhập số trẻ tối đa"
+                    {...register("members")}
+                  />
+                  {errors.members && (
+                    <p className="text-destructive text-sm">
+                      {errors.members.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="tuition" className="text-sm block">
+                    Học phí
+                  </Label>
+                  <Input
+                    type="text"
+                    id="tuition"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Nhập học phí"
+                    {...register("tuition")}
+                  />
+                  {errors.tuition && (
+                    <p className="text-destructive text-sm">
+                      {errors.tuition.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <Label htmlFor="schedule" className="text-sm block">
+                    Lịch học
+                  </Label>
+                  <Input
+                    type="text"
+                    id="schedule"
+                    className="rounded-xl border border-[#000000] shadow-md"
+                    placeholder="Mô tả lịch học"
+                    {...register("schedule")}
+                  />
+                  {errors.schedule && (
+                    <p className="text-destructive text-sm">
+                      {errors.schedule.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full flex justify-center">
+                  <Button
+                    type="submit"
+                    className="rounded-2xl bg-[#05D988] hover:bg-[#02B671] focus:bg-[#05D988]  transition all"
+                    disabled={isSubmitting}
+                  >
+                    Tạo lớp học
+                  </Button>
+                </div>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <ul className="grid grid-cols-4 gap-8 mt-4">
+        {classes.map((mainclass) => (
+          <li>
+            <form onSubmit={had(onSubmit2)} className="rounded-xl bg-[#ffffff] shadow-md flex flex-wrap ">
+               <Input type="hidden" id="classid" value={mainclass.classid} {...reg("classid")}/>
+               <div className="w-full h-25 overflow-hidden mb-3">
+                  <img src={mainclass.imageurl} alt="anh lop hoc" className="w-full object-cover"/>
+               </div>
+               <div className="px-8 gap-4">
       
-    </div>
-  )
-}
+                     <h2 className="text-md font-bold">Tên lớp: {mainclass.name}</h2>
+                     <h2 className="text-md font-bold">Giáo viên: {mainclass.teachername}</h2>
+                     <h2 className="text-md font-bold">Độ tuổi: {mainclass.age}</h2>
+                     <h2 className="text-md font-bold">Số lượng trẻ: {mainclass.members}</h2>
+                     <h2 className="text-md font-bold">Học phí: {mainclass.tuition}</h2>
+                     <h2 className="text-md font-bold">Tình trạng: {`${mainclass.currentmambers}/${mainclass.members}`}</h2>
+               
 
-export default Class
+               </div>
+               <div className="w-full px-8 flex justify-end">
+                  <Button type="submit" className="mt-3 mb-3 bg-[#f52121] rounded-2xl shadow-sm hover:bg-[#9E0C0C] focus:bg-[#f52121] transition all" disabled={isSub}>Xóa</Button>
+               </div>
+            </form>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Class;
--- frontend/src/components/admin/Notification.tsx
+++ frontend/src/components/admin/Notification.tsx
@@ -9,7 +9,8 @@ import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 const NotificationFormSchema = z.object({
     title: z.string().min(1,"Tiêu đề không được để trống"),
-    content: z.string().min(1,"Nội dung không được để trống")
+    content: z.string().min(1,"Nội dung không được để trống"),
+    receiver: z.string()
 })
 type NotificationFormValues = z.infer<typeof NotificationFormSchema>
 type NotificationCard = {
@@ -20,6 +21,36 @@ type NotificationCard = {
   createdat: Date,
 }
 const Notification = () => {
+  const notifications = [
+    {
+      notificationid : '001',
+      receiver: 'phụ huynh',
+      title: 'Đóng tiền học',
+      content:'Hiện đã đến kì đóng tiền học, yêu cầu phụ huynh nhanh chóng đóng tiền cho con về phía nhà trường a',
+      createat:'2026-01-01'
+    },
+    {
+      notificationid : '002',
+      receiver: 'phụ huynh',
+      title: 'Đóng tiền học',
+      content:'Hiện đã đến kì đóng tiền học, yêu cầu phụ huynh nhanh chóng đóng tiền cho con về phía nhà trường a',
+      createat:'2026-01-01'
+    },
+    {
+      notificationid : '003',
+      receiver: 'phụ huynh',
+      title: 'Đóng tiền học',
+      content:'Hiện đã đến kì đóng tiền học, yêu cầu phụ huynh nhanh chóng đóng tiền cho con về phía nhà trường a',
+      createat:'2026-01-01'
+    },
+    {
+      notificationid : '004',
+      receiver: 'phụ huynh',
+      title: 'Đóng tiền học',
+      content:'Hiện đã đến kì đóng tiền học, yêu cầu phụ huynh nhanh chóng đóng tiền cho con về phía nhà trường a',
+      createat:'2026-01-01'
+    },
+  ]
   const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<NotificationFormValues>({
     resolver : zodResolver(NotificationFormSchema)
   })
@@ -28,8 +59,10 @@ const Notification = () => {
 
   }
   const onSubmit2 = async (data: NotificationFormValues) =>{
-
+      const {title,content,receiver} = data;
+      console.log(title + ' ' + content +' ' +receiver)
   }
+
   return (
     <>
       <div className='flex justify-between flex-wrap'>
@@ -43,7 +76,7 @@ const Notification = () => {
           <form className='w-full gap-6 flex flex-wrap ' onSubmit={handleSubmit(onSubmit2)}>
 
             <h2 className='text-lg font-medium'>Người nhận:</h2>
-            <RadioGroup defaultValue='Giáo viên' id='receiver' className='flex gap-3 '>
+            <RadioGroup defaultValue='Giáo viên' id='receiver' className='flex gap-3' {...register("receiver")}>
               <div className='flex items-center gap-2'>
                 <RadioGroupItem id='teacher' value='Giáo viên'/>
                 <Label htmlFor='teacher' className='text-base'>Giáo viên</Label>
@@ -75,8 +108,28 @@ const Notification = () => {
           
       
     </div>
-    <ul>
-
+    <ul className='mt-4'>
+      {notifications.map(notification => (
+        <li className='mb-4'>
+          <form className='grid grid-cols-2 justify-between gap-6 rounded-2xl w-full shadow-md bg-[#ffffff] px-4 py-2' onSubmit={had(onSubmit1)}>
+            <div>
+              <Input type='hidden' id='notificationid' value={notification.notificationid} {...reg("notificationid")}/>
+              <Input type='hidden' id='title' value={notification.title} {...reg("title")}/>
+              <Input type='hidden' id='content' value={notification.content} {...reg("content")}/>
+              <Input type='hidden' id='createdat' value={notification.createat} {...reg("createdat")}/>
+              <Input type='hidden' id='receiver' value={notification.receiver} {...reg("receiver")}/>
+              <h1 className='text-xl font-medium block'>Người nhận: {notification.receiver}</h1>
+              <h1 className='text-xl font-medium block'>Tiêu đề: {notification.title}</h1>
+              <h1 className='text-xl font-medium block'>Nội dung: {notification.content}</h1>
+              <h1 className='text-xl font-medium block'>ngày gửi: {notification.createat}</h1>
+            </div>
+            <div className='flex justify-end items-center'>
+              <Button type='submit' className='w-20 bg-[#f52121] rounded-2xl shadow-sm hover:bg-[#9E0C0C] focus:bg-[#f52121] transition all' disabled={isSub} >Xóa</Button>
+            </div>
+            
+          </form>
+        </li>
+      ))}
     </ul>
     </>
   )
