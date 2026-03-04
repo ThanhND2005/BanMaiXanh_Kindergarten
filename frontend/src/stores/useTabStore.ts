199bca9852b6d1f9fa10e20b3cffe87fb5fc793d
danhthanh <nguyendanht2005@gmail.com>
2026-03-04 19:49:49 +0700

    update 4/3/2026

--- frontend/src/stores/useTabStore.ts
+++ frontend/src/stores/useTabStore.ts
@@ -1,24 +1,41 @@
 import {create} from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 import { type tabTeacherState, type tabAdminState,type tabParentState } from '@/types/store'
-export const useTabAdminStore = create<tabAdminState>((set) =>({
+export const useTabAdminStore = create<tabAdminState>()(
+    persist(
+    (set, get) => ({
+      tabActive: 'dashboard', 
+      setTabActive: (tab) => set({ tabActive: tab }),
+    }),
+    {
+      name: 'tab-admin-storage', 
+    }
+  )
+)
+export const useTabTeacherStore = create<tabTeacherState>()(
+    persist(
+    (set,get) =>({
     tabActive: 'dashboard',
     setTabActive : (tab) =>{
         set({tabActive : tab})
     }
+}),{
+    name:'tab-teacher-storage'
 }))
-export const useTabTeacherStore = create<tabTeacherState>((set) =>({
-    tabActive: 'dashboard',
-    setTabActive : (tab) =>{
-        set({tabActive : tab})
-    }
-}))
-export const useTabParentStore = create<tabParentState>((set) =>({
-    tabActive: 'dashboard',
-    student : null,
-    setStudent : (student) => {
-        set({student})
-    },
-    setTabActive :(tab) =>{
-        set({tabActive : tab})
-    }
-}))
+export const useTabParentStore = create<tabParentState>()(
+    persist(
+
+        (set) =>({
+        tabActive: 'dashboard',
+        student : null,
+        setStudent : (student) => {
+            set({student})
+        },
+        setTabActive :(tab) =>{
+            set({tabActive : tab})
+        }}),
+        {
+            name:'tab-student-storage'
+        }
+ ))
+    
